f4aae5deb6dff94290a1a95e1bc7b0f926af8375
himu62 <zero.256dic@gmail.com>
2017-05-14 19:53:38 +0900

    update

--- server/vlbv.go
+++ server/vlbv.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+)
+
+func main() {
+	walk.MainWindow{
+		Title:   "Title",
+		MinSize: walk.Size{480, 320},
+		Layout:  walk.VBox{},
+		children: []walk.Widget{
+			walk.PushButton{
+				Text: "Button",
+				OnClicked: func() {
+					log.Println("clicked")
+				},
+			},
+		},
+	}.Run()
 }
